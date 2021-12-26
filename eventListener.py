5b8e6301803ace9d22ff4b2e485696fedd477e0d
nolanjannotta <anjannotta@verizon.net>
2021-12-25 23:00:46 -0500

    cleaup

--- eventListener.py
+++ eventListener.py
@@ -14,16 +14,13 @@ web3 = Web3(Web3.HTTPProvider(apiURL))
 f = open('./ABI/EmitEvent.json')
 abi = json.load(f)
 
-# contract instance
+
 EmitEvent = web3.eth.contract(address=contractAddress, abi=abi)
 
 # filter for contract address
 block_filter = web3.eth.filter({'fromBlock':'latest', 'address':contractAddress})
 
-
 balance = web3.eth.getBalance(account)
-
-
 # event object
 newString_Event = EmitEvent.events.NewString()
 
@@ -39,4 +36,5 @@ def event_loop(event_filter, poll_interval):
             time.sleep(poll_interval)
 
 print("listening for events...")
+
 event_loop(block_filter, 2)
